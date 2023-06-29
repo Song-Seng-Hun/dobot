55ebab002589b3b29a5e7880e76fbf6297f58ff4
송승훈 <69943723+Song-Seng-Hun@users.noreply.github.com>
2023-06-29 16:21:36 +0900

    Add files via upload

---
+++ DobotYOLO.py
@@ -0,0 +1,40 @@
+import DobotYOLOv3.yolov3.DobotDetect as DD
+# from DobotDetect import *
+
+
+def get_results():
+
+    # result = DobotDetect.run()
+    result = DD.run()
+
+
+    c_dict = {'0': 'red', '1': 'yellow', '2': 'green'}
+    s_dict = {'0': 'Hexagonal Prism', '1': 'Cylinder', '2': 'Cube'}
+
+
+    result_list = []
+    for boxes in result:
+
+        w = float(boxes.split()[-2])
+        h = float(boxes.split()[-1])
+
+        for i, v in enumerate(boxes.split()):
+            if i == 0:
+                color = c_dict[v]
+                shape = s_dict[v]
+            elif i == 1:
+                x = int(float(v) * w)
+            elif i == 2:
+                y = int(float(v) * h)
+
+        result_list.append([color, shape, x, y])
+                                  
+    print(result_list)
+    return result_list
+
+
+def main():
+    get_results()
+
+if __name__ == "__main__":
+    main()
